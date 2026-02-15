2220f4d452d55312f9063612dcdea67eafe029ba
MOHIT TIWARI <163675793+mohit23ucs172@users.noreply.github.com>
2026-02-15 22:46:17 +0530

    feat: Set base path in Vite configuration for consistent asset loading

--- lansync-ui/vite.config.js
+++ lansync-ui/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   server: {
     host: true,  // 🟢 THIS IS THE KEY! It exposes the app to your Wi-Fi/Hotspot
